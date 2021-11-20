3f5dd33cf4088f0854686bb0e6f19cb0840127d3
Irem Gunay <iremguunay@gmail.com>
2021-11-20 12:39:21 +0300

    While ile çoklu şart örneği eklendi

--- src/com/bilgeadam/sayisalloto/CokluSartOrnek.java
+++ src/com/bilgeadam/sayisalloto/CokluSartOrnek.java
@@ -7,11 +7,18 @@ public class CokluSartOrnek {
         int sayi1 = 23;
         int sayi2 = 35;
 
-        int sayi3 = 23;
+        int sayi3 = 41;
 
-        // veya(or) operatörü  ||
+        // veya(or) operatörü  || - ilkinde sağlıyorsa ikincisine bakmana gerek yok
+        // | - sadece bu işaret de veya anlamına gelir
         if (sayi3 == sayi1 || sayi3 == sayi2) {
             System.out.println("Tekrarlanan sayı var.");
+        }
+
+        while(sayi3 != sayi1 && sayi3 != sayi2) {
+
+            System.out.println("Sayi3 diğerlerine eşit değil!");
+            sayi3 = sayi1;
         }
 
     }
