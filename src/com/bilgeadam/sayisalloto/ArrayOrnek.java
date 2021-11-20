6901aee60a73fcc164a4f9ca3890dc8daccdc6ed
Irem Gunay <iremguunay@gmail.com>
2021-11-20 12:16:13 +0300

    Array örnek eklendi

---
+++ src/com/bilgeadam/sayisalloto/ArrayOrnek.java
@@ -0,0 +1,29 @@
+package com.bilgeadam.sayisalloto;
+
+public class ArrayOrnek {
+
+    public static void main(String[] args) {
+
+        //arraylerin tipi olmak zorunda
+        int sayilar[] = new int[6];
+
+        //6'lı oluşturduysak, array indeksleri 0'dan 5'e kadardır
+
+        sayilar[0] = 23;
+        sayilar[1] = 11;
+        sayilar[2] = -35;
+        sayilar[3] = 46;
+        sayilar[4] = 0;
+        sayilar[5] = -29;
+
+        //Bu hata verir, 6 diye bir indeks yok.
+        //sayilar[6] = 45;
+
+        //arrayi yazdıralım
+        for(int i=0; i < 6; i++) {
+            System.out.println(sayilar[i]);
+        }
+
+    }
+
+}
