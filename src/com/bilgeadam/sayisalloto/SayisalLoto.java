06daf9b6791d1df7c5b57fbd151fdb8f1d045c46
Irem Gunay <iremguunay@gmail.com>
2021-11-20 13:50:20 +0300

    Sonuç yazdırıldı

--- src/com/bilgeadam/sayisalloto/SayisalLoto.java
+++ src/com/bilgeadam/sayisalloto/SayisalLoto.java
@@ -1,5 +1,6 @@
 package com.bilgeadam.sayisalloto;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class SayisalLoto {
@@ -16,9 +17,29 @@ public class SayisalLoto {
         //6'lı bir array oluşturduk
         int sayilar[] = new int[6];
 
+        int temp_sayi = 0;
+
+        int sonuc = 0;
+
         //döngüyle array'i dolduralım
         for (int i = 0; i < 6; i++) {
-            sayilar[i] = loto.sayiGetir(49);
+
+
+            while(sonuc >= 0) {
+
+                temp_sayi = loto.sayiGetir(49);
+
+                // array'i sıralayın
+                Arrays.sort(sayilar);
+
+                //binarySearch kullanmanın şartı, array'in sıralı olmasıdır
+                sonuc = Arrays.binarySearch(sayilar, temp_sayi);
+
+            }
+
+            sayilar[1] = temp_sayi;
+            sonuc = 0;
+
         }
 
         //array'i yazdır
