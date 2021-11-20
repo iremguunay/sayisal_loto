b6953d56cefd1cc1e4e84f617e2fb227b8eeb801
Irem Gunay <iremguunay@gmail.com>
2021-11-20 14:50:51 +0300

    sayisal loto array örneği yapıldı

--- src/com/bilgeadam/sayisalloto/SayisalLoto_array.java
+++ src/com/bilgeadam/sayisalloto/SayisalLoto_array.java
@@ -34,34 +34,63 @@ public class SayisalLoto_array {
 
     }
 
-    public void testArraydeVarMi () {
-        int sayilar[] = {13,35,67,-4,41,70};
-
-        int sayi = 68;
-
-        boolean sonuc = arraydeVarMi(sayilar, sayi);
-
-        if (sonuc == true)
-            System.out.println("Array'de var!");
-        else
-            System.out.println("Array'de yok!");
-    }
+//    public void testArraydeVarMi () {
+//        int sayilar[] = {13,35,67,-4,41,70};
+//
+//        int sayi = 68;
+//
+//        boolean sonuc = arraydeVarMi(sayilar, sayi);
+//
+//        if (sonuc == true)
+//            System.out.println("Array'de var!");
+//        else
+//            System.out.println("Array'de yok!");
+//    }
 
     public static void main(String[] args) {
         //Yeni çılgın sayısal loto'ya uygun.
         //90 sayıdan 6 tane seçeceğiz.
         //https://www.millipiyangoonline.com/cekilis-sonuclari#sayisaloto
-        
-        //sayıları tutacağımız bir array tanımlıyoruz
-       // int sayilar[] = new int[KAC_SAYI];
-        
-        //Döngü ile array'e rastgele sayı atıyoruz
-       // for (int i = 0; i < sayilar.length; i++) {
-
-        //}
 
         SayisalLoto_array loto = new SayisalLoto_array();
-        loto.testArraydeVarMi();
+        
+        //sayıları tutacağımız bir array tanımlıyoruz
+         int sayilar[] = new int[KAC_SAYI];
+
+        //Döngü ile array'e rastgele sayı atıyoruz
+        for (int i = 0; i < sayilar.length; i++) {
+
+            int rastgeleSayi = loto.sayiGetir();
+
+            //rastgele sayı arrayde varsa döngüyle tekrar sayı alınacak.
+            //arrayde olmayan sayıyı bulana kadar döngüden çıkamaz.
+            boolean sonuc = loto.arraydeVarMi(sayilar, rastgeleSayi);
+            while (sonuc == true) {
+                rastgeleSayi = loto.sayiGetir();
+                sonuc = loto.arraydeVarMi(sayilar, rastgeleSayi);
+            }
+
+            //Hep array'in ilk elemanına yazalım sayıyı
+            //Çünkü sıralanınca sayı sonlara gidiyor
+            sayilar[0] = rastgeleSayi;
+
+            //6. sayıyı sıfır indekse yazınca, tekrar sıralanması için bunu ekledik
+            Arrays.sort(sayilar);
+
+        }
+
+        //arrayi yazdır
+        for (int i = 0; i < sayilar.length; i++) {
+
+            if (i == sayilar.length -1)
+                System.out.println(sayilar[i]);
+            else
+                System.out.print(sayilar[i] + "-");
+
+        }
+
+//        SayisalLoto_array loto = new SayisalLoto_array();
+//        loto.testArraydeVarMi();
 
     }
 }
